be83504ba2229c424294a2de3220cdf45943aa4c
Miles Lasater <miles@mileslasater.com>
2014-12-29 16:46:10 +0000

    did homework

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a complete ninja now');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('fozzy bear');
 });
 
 app.listen(
